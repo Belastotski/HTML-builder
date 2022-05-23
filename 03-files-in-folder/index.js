2aa0452180e92401c7a15b518be5fb501546048e
Belastotski <di.belost@gmail.com>
2022-05-23 15:01:10 +0200

    feat: task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const { resolve, join, parse } = require('path');
+const { readdir , stat } = require('fs').promises;
+
+let path = join(__dirname, 'secret-folder');
+
+async function* files(path) {
+  const dirs = await readdir(path, { withFileTypes: true });
+  for (const dir of dirs) {
+    const res = resolve(path, dir.name);
+    dir.isDirectory() ? yield* files(res) : yield res;
+  }
+}
+
+(async () => {
+  for await (const file of files(path)) {
+    let {ext,name} = parse(file);
+    ext = ext? ext.slice(1, ext.length): '';
+    const {size} = await stat(file);
+    console.log(`${name} - ${ext} - ${size > 1024 ? size/1024 +'kb' : size + 'b' }`);
+  }
+})();
+
